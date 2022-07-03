110c7eb7ffa2c1d07fe3185ce49c8e3c47135b65
B17_Shishani <83888130+MoadBino@users.noreply.github.com>
2022-07-03 22:57:08 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
 import Welcome from "./welcomepage";
 import Aboutme from "./aboutme/aboutme";
 import { Route, Routes } from "react-router-dom";
+import me from "./phhoto/me.png";
+import me2 from "./phhoto/m3.jpg";
 function App() {
   return (
     <div className="App">
+      <div className="mainfooter">
+        <div className="GeeksForGeeks">
+
+          <div className="intro">
+            <div className="introdetalis">
+              <h1 className="name">Moath al-shishani</h1>
+              <h3>A Creative Full Stack Developer</h3>
+            </div>
+            <div>
+              <img className="self" src={me}></img>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="main1">
+        <div className="pae2mainn">
+          <img src={me2}></img>
+          <div className="maindetalis">
+            <div>
+              <h1>ABOUT ME </h1>
+              <hr className="hr"></hr>
+            </div>
+            <h2>
+              {" "}
+              Full-stack developer with solid experience in building responsive
+              web applications and design. Strive for excellence in everything I
+              do. Interested to work for a company that encourages innovative
+              thinking and enables me to utilize my skills and abilities.
+            </h2>
+          </div>
+        </div>
+      </div>
+      <div className="skilss">
+        <div className="mainskills">
+          <h1>My Skills</h1>
+        </div>
+        <div className="skils">
+          <h2>* HTML</h2>
+          <h2>* CSS</h2>
+          <h2>* JAVASCRIPT</h2>
+          <h2>* JQUERY</h2>
+          <h2>* DOM</h2>
+          <h2>* node.js</h2>
+          <h2>* Express</h2>
+          <h2>* React</h2>
+          <h2>* Redux</h2>
+          <h2>* mongoose</h2>
+          <h2>* Mysql</h2>
+          <h2>* Familiar with Github</h2>
+        </div>
+      </div>
       <Routes>
-        <Route path="/" element={<Welcome />} />
+        <Route path="jj" element={<Welcome />} />
         <Route path="/aboutme" element={<Aboutme />} />
       </Routes>
     </div>
--- src/aboutme/aboutme.js
+++ src/aboutme/aboutme.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
+import self from "./img/./me.jpg";
+import glf from "./img/200.gif";
+import glf2 from "./img/300.gif";
+import glf3 from "./img/400.gif";
+import "./style.css";
 
 export default function Aboutme() {
+  const [glfclas, setGlfclas] = useState("");
+  const [disply, setdisply] = useState("none");
+  setTimeout(() => {
+    setGlfclas("glfclass");
+    setdisply("flex");
+  }, "5000");
   return (
     <div>
-        
+      <div className="header">
+        <h1>let me introduce myself </h1>
+      </div>
+      <div className="imgcontenr">
+        <div className="first">
+          <div className="child">
+            <h2>
+              Full-stack developer with solid experience in building responsive
+              web applications and design. Strive for excellence in everything I
+              do.
+            </h2>
+            <div className="test" style={{ display: `${disply}` }}>
+              <img className={glfclas} src={glf}></img>
+              <img className={glfclas} src={glf2}></img>
+              <img className={glfclas} src={glf3}></img>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
+
+
